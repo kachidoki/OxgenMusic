69cc94febb8c22045ffd59d5797ab02dbea0c8f2
mayiwei <mayiwei889@126.com>
2017-01-20 19:40:33 +0800

    bug

---
+++ app/src/main/java/com/kachidoki/oxgenmusic/activity/LocalActivity.java
@@ -0,0 +1,19 @@
+package com.kachidoki.oxgenmusic.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.kachidoki.oxgenmusic.app.BaseActivity;
+
+/**
+ * Created by mayiwei on 17/1/23.
+ */
+public class LocalActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
